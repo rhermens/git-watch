b38be55b8cee33bd58d455482833410f12334b0f
Roy <royhermens@hotmail.com>
2024-07-19 07:38:02 +0200

    Cleanup

--- git-watch.c
+++ git-watch.c
@@ -27,9 +27,10 @@ git_repository *REPOSITORY;
 git_remote *REMOTE;
 git_checkout_options CHECKOUT_OPTIONS = GIT_CHECKOUT_OPTIONS_INIT;
 git_fetch_options FETCH_OPTIONS = GIT_FETCH_OPTIONS_INIT;
+git_commit_create_options COMMIT_OPTIONS = GIT_COMMIT_CREATE_OPTIONS_INIT;
+git_push_options PUSH_OPTIONS = GIT_PUSH_OPTIONS_INIT;
 
 int cred_cb(git_cred **out, const char *url, const char *username_from_url, unsigned int allowed_types, void *payload) {
-    printf("Logging in\n");
     const char *homedir;
 
     if ((homedir = getenv("HOME")) == NULL) {
@@ -46,78 +47,51 @@ int cred_cb(git_cred **out, const char *url, const char *username_from_url, unsi
 }
 
 int fetchhead_cb_merge(const char *ref_name, const char *remote_url, const git_oid *oid, unsigned int is_merge, void *payload) {
-    int error;
-    const git_error *e;
+    int status = 0;
     git_annotated_commit *fetch_commit;
     git_merge_analysis_t analysis;
     git_merge_preference_t preference;
     git_object *target;
     git_reference *target_ref;
     git_reference *new_target_ref;
-    const git_annotated_commit **their_head;
 
-    error = git_annotated_commit_lookup(&fetch_commit, REPOSITORY, oid);
-    if (error != 0) {
-        e = git_error_last();
-        printf("Error %d/%d: %s\n", error, e->klass, e->message);
-        return error;
+    if ((status = git_annotated_commit_lookup(&fetch_commit, REPOSITORY, oid)) != 0) {
+        goto done;
     }
 
-    error = git_repository_head(&target_ref, REPOSITORY);
-    if (error != 0) {
-        e = git_error_last();
-        printf("Error %d/%d: %s\n", error, e->klass, e->message);
-        return error;
+    if((status = git_repository_head(&target_ref, REPOSITORY)) != 0) {
+        goto done;
     }
 
-    their_head = (const git_annotated_commit **)&fetch_commit;
-    error = git_merge_analysis(&analysis, &preference, REPOSITORY, their_head, 1);
-    if (error != 0) {
-        e = git_error_last();
-        printf("Error %d/%d: %s\n", error, e->klass, e->message);
-        return error;
+    if ((status = git_merge_analysis(&analysis, &preference, REPOSITORY, (const git_annotated_commit **)&fetch_commit, 1)) != 0) {
+        goto done;
     }
 
-    if (analysis & GIT_MERGE_ANALYSIS_UP_TO_DATE) {
-        printf("Already up to date\n");
-        return 0;
+    if (analysis & GIT_MERGE_ANALYSIS_UP_TO_DATE || !(analysis & GIT_MERGE_ANALYSIS_FASTFORWARD)) {
+        goto done;
     }
 
-    if (!(analysis & GIT_MERGE_ANALYSIS_FASTFORWARD)) {
-        printf("Fast-forward impossible\n");
-        return 1;
+    if ((status = git_object_lookup(&target, REPOSITORY, oid, GIT_OBJECT_COMMIT)) != 0) {
+        goto done;
     }
 
-    printf("Fast-forwarding..");
-    error = git_object_lookup(&target, REPOSITORY, oid, GIT_OBJECT_COMMIT);
-    if (error != 0) {
-        e = git_error_last();
-        printf("Error %d/%d: %s\n", error, e->klass, e->message);
-        return error;
+    if ((status = git_checkout_tree(REPOSITORY, target, &CHECKOUT_OPTIONS)) != 0) {
+        goto done;
     }
 
-    error = git_checkout_tree(REPOSITORY, target, &CHECKOUT_OPTIONS);
-    if (error != 0) {
-        e = git_error_last();
-        printf("Error %d/%d: %s\n", error, e->klass, e->message);
-        return error;
+    if ((status = git_reference_set_target(&new_target_ref, target_ref, oid, NULL)) != 0) {
+        goto done;
     }
 
-    error = git_reference_set_target(&new_target_ref, target_ref, oid, NULL);
-    if (error != 0) {
-        e = git_error_last();
-        printf("Error %d/%d: %s\n", error, e->klass, e->message);
-        return error;
-    }
-
+done:
+    git_annotated_commit_free(fetch_commit);
     git_reference_free(target_ref);
     git_reference_free(new_target_ref);
     git_object_free(target);
-
-    return 0;
+    return status;
 }
 
-int status_cb(const char *path, unsigned int status_flags, void *payload) {
+int status_foreach_cb(const char *path, unsigned int status_flags, void *payload) {
     int error;
     git_index *index = (git_index *)payload;
 
@@ -153,7 +127,6 @@ int status_cb(const char *path, unsigned int status_flags, void *payload) {
 
 int do_fastforward() {
     int error;
-
     error = git_remote_fetch(REMOTE, NULL, &FETCH_OPTIONS, NULL);
     if (error != 0) {
         const git_error *e = git_error_last();
@@ -177,94 +150,87 @@ int push_transfer_cb(unsigned int i, unsigned int j, unsigned long k, void *payl
 }
 
 int do_push() {
-    printf("Doing push maybe..\n");
-    int error;
+    int status = 0;
     git_object *head_commit;
     git_reference *head_reference;
     git_oid head_oid, tree_oid, commit_oid;
     git_index *index;
-
-    error = git_revparse_ext(&head_commit, &head_reference, REPOSITORY, "HEAD");
-    if (error != 0) {
-        printf("Error %d: %s\n", error, git_error_last()->message);
-        return error;
-    }
-
-    error = git_repository_index(&index, REPOSITORY);
-    if (error != 0) {
-        printf("Error %d: %s\n", error, git_error_last()->message);
-        return error;
-    }
-
-    git_status_foreach(REPOSITORY, status_cb, index);
-
-    error = git_index_write(index);
-    if (error != 0) {
-        printf("Oof, Error %d: %s\n", error, git_error_last()->message);
-        return error;
-    }
-
-    git_commit_create_options commit_opts = GIT_COMMIT_CREATE_OPTIONS_INIT;
-    error = git_commit_create_from_stage(&commit_oid, REPOSITORY, "Autocommit: Push commit", &commit_opts);
-    if (error != 0) {
-        printf("Error %d: %s\n", error, git_error_last()->message);
-        return error;
-    }
-
-    git_push_options push_options = GIT_PUSH_OPTIONS_INIT;
-    push_options.callbacks.credentials = cred_cb;
     char *ref = "refs/heads/master";
     git_strarray refspecs = { &ref, 1 };
-    error = git_remote_push(REMOTE, &refspecs, &push_options);
 
-    if (error != 0) {
-        printf("Error %d: %s\n", error, git_error_last()->message);
+    if ((status = git_revparse_ext(&head_commit, &head_reference, REPOSITORY, "HEAD")) != 0) {
+        goto done;
+    }
+
+    if ((status = git_repository_index(&index, REPOSITORY)) != 0) {
+        goto done;
+    }
+
+    git_status_foreach(REPOSITORY, status_foreach_cb, index);
+
+    if ((status = git_index_write(index)) != 0) {
+        goto done;
+    }
+
+    if ((status = git_commit_create_from_stage(&commit_oid, REPOSITORY, "Autocommit: Push commit", &COMMIT_OPTIONS)) != 0) {
+        goto done;
+    }
+
+    if ((status = git_remote_push(REMOTE, &refspecs, &PUSH_OPTIONS)) != 0) {
+        goto done;
+    }
+
+done:
+    git_object_free(head_commit);
+    git_reference_free(head_reference);
+    git_index_free(index);
+
+    return status;
+}
+
+int open_repository(char *path) {
+    git_libgit2_init();
+    int error;
+
+    if ((error = git_repository_open(&REPOSITORY, path)) != 0) {
         return error;
     }
 
-    git_index_free(index);
+    return git_remote_lookup(&REMOTE, REPOSITORY, "origin");
+}
 
-    return 0;
+void setup_options() {
+    CHECKOUT_OPTIONS.checkout_strategy = GIT_CHECKOUT_SAFE;
+    FETCH_OPTIONS.callbacks.credentials = cred_cb;
+    PUSH_OPTIONS.callbacks.credentials = cred_cb;
 }
 
 int main(int args, char** argv) {
     if (args != 2) {
-        printf("Usage: autosync <path>");
-        return 1;
+        printf("Usage: %s <path>", argv[0]);
+        exit(1);
     }
 
     struct stat repository_stat;
     stat(argv[1], &repository_stat);
     if (!(repository_stat.st_mode & S_IFDIR)) {
         printf("Path does not exist");
-        return 1;
-    }
-    chdir(argv[1]);
-
-    git_libgit2_init();
-    FETCH_OPTIONS.callbacks.credentials = cred_cb;
-    CHECKOUT_OPTIONS.checkout_strategy = GIT_CHECKOUT_SAFE;
-    int error;
-
-    error = git_repository_open(&REPOSITORY, argv[1]);
-    if (error != 0) {
-        const git_error *e = git_error_last();
-        printf("Error %d/%d: %s\n", error, e->klass, e->message);
-        exit(error);
+        exit(1);
     }
 
-    error = git_remote_lookup(&REMOTE, REPOSITORY, "origin");
-    if (error != 0) {
-        const git_error *e = git_error_last();
-        printf("Error %d/%d: %s\n", error, e->klass, e->message);
-        exit(error);
-    }
-
-    while (1) {
+    int status = 0;
+    status = open_repository(argv[1]);
+    setup_options();
+    while (status == 0) {
         printf("Watching..\n");
-        do_fastforward();
-        do_push();
+        status = do_fastforward();
+        status = do_push();
+
         git_repository_state_cleanup(REPOSITORY);
         sleep(60);
     }
+
+    const git_error *e = git_error_last();
+    printf("Error %d/%d: %s\n", status, e->klass, e->message);
+    return status;
 }
