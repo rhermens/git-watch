5f2df1176dcbfa13712a1d428221a6d8ba099ffd
Roy <royhermens@hotmail.com>
2024-07-20 06:47:13 +0200

    Cleanup

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 CC=gcc
 
 git-watch: git-watch.c
-	$(CC) -o git-watch git-watch.c -I. -lgit2
+	$(CC) -o git-watch git-watch.c -I. -lgit2 -pedantic-errors -Wall -std=c11
--- git-watch.c
+++ git-watch.c
@@ -15,6 +15,7 @@
 #include <git2/status.h>
 #include <git2/tree.h>
 #include <git2/types.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <git2.h>
@@ -30,6 +31,13 @@ git_fetch_options FETCH_OPTIONS = GIT_FETCH_OPTIONS_INIT;
 git_commit_create_options COMMIT_OPTIONS = GIT_COMMIT_CREATE_OPTIONS_INIT;
 git_push_options PUSH_OPTIONS = GIT_PUSH_OPTIONS_INIT;
 
+void verbose_print(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+}
+
 int cred_cb(git_cred **out, const char *url, const char *username_from_url, unsigned int allowed_types, void *payload) {
     const char *homedir;
 
@@ -37,8 +45,8 @@ int cred_cb(git_cred **out, const char *url, const char *username_from_url, unsi
         homedir = getpwuid(getuid())->pw_dir;
     }
 
-    char pubkey[sizeof(homedir) + 20] = {};
-    char privkey[sizeof(homedir) + 16] = {};
+    char pubkey[sizeof(homedir) + 20];
+    char privkey[sizeof(homedir) + 16];
 
     sprintf(pubkey, "%s/.ssh/id_ed25519.pub", homedir);
     sprintf(privkey, "%s/.ssh/id_ed25519", homedir);
@@ -48,12 +56,12 @@ int cred_cb(git_cred **out, const char *url, const char *username_from_url, unsi
 
 int fetchhead_cb_merge(const char *ref_name, const char *remote_url, const git_oid *oid, unsigned int is_merge, void *payload) {
     int status = 0;
-    git_annotated_commit *fetch_commit;
+    git_annotated_commit *fetch_commit = NULL;
     git_merge_analysis_t analysis;
     git_merge_preference_t preference;
-    git_object *target;
-    git_reference *target_ref;
-    git_reference *new_target_ref;
+    git_object *target = NULL;
+    git_reference *target_ref = NULL;
+    git_reference *new_target_ref = NULL;
 
     if ((status = git_annotated_commit_lookup(&fetch_commit, REPOSITORY, oid)) != 0) {
         goto done;
@@ -92,68 +100,43 @@ done:
 }
 
 int status_foreach_cb(const char *path, unsigned int status_flags, void *payload) {
-    int error;
-    git_index *index = (git_index *)payload;
-
     if (status_flags & GIT_STATUS_WT_DELETED) {
-        error = git_index_remove_bypath(index, path);
-        return error;
+        return git_index_remove_bypath((git_index *)payload, path);
     }
 
     time_t curr_time;
     struct stat st;
     time(&curr_time);
 
-    error = stat(path, &st);
-    if (error != 0) {
-        printf("Error %d\n", error);
-        return error;
-    }
-
-    int msec = curr_time - st.st_mtim.tv_sec;
-    if (msec < 60) {
-        printf("File modified less than a minute ago: %s\n", path);
+    if (stat(path, &st) != 0) {
+        verbose_print("Error statting file %d\n", path);
         return 0;
     }
 
-    error = git_index_add_bypath(index, path);
-    if (error != 0) {
-        printf("Error %d: %s\n", error, git_error_last()->message);
-        return error;
+    int msec = curr_time - st.st_mtime;
+    if (msec < 60) {
+        verbose_print("File modified less than a minute ago: %s\n", path);
+        return 0;
     }
 
-    return 0;
+    return git_index_add_bypath((git_index *)payload, path);
 }
 
 int do_fastforward() {
     int error;
-    error = git_remote_fetch(REMOTE, NULL, &FETCH_OPTIONS, NULL);
-    if (error != 0) {
-        const git_error *e = git_error_last();
-        printf("Error %d/%d: %s\n", error, e->klass, e->message);
+
+    if ((error = git_remote_fetch(REMOTE, NULL, &FETCH_OPTIONS, NULL)) != 0) {
         return error;
     }
 
-    git_repository_fetchhead_foreach(REPOSITORY, fetchhead_cb_merge, NULL);
-
-    return 0;
-}
-
-int transfer_cb(const git_transfer_progress *stats, void *payload) {
-    printf("Transferred %d/%d\n", stats->received_objects, stats->total_objects);
-    return 0;
-}
-
-int push_transfer_cb(unsigned int i, unsigned int j, unsigned long k, void *payload) {
-    printf("Pushed something\n");
-    return 0;
+    return git_repository_fetchhead_foreach(REPOSITORY, fetchhead_cb_merge, NULL);
 }
 
 int do_push() {
     int status = 0;
     git_object *head_commit;
     git_reference *head_reference;
-    git_oid head_oid, tree_oid, commit_oid;
+    git_oid commit_oid;
     git_index *index;
     char *ref = "refs/heads/master";
     git_strarray refspecs = { &ref, 1 };
@@ -166,7 +149,9 @@ int do_push() {
         goto done;
     }
 
-    git_status_foreach(REPOSITORY, status_foreach_cb, index);
+    if ((status = git_status_foreach(REPOSITORY, status_foreach_cb, index)) != 0) {
+        goto done;
+    }
 
     if ((status = git_index_write(index)) != 0) {
         goto done;
@@ -196,6 +181,8 @@ int open_repository(char *path) {
         return error;
     }
 
+    chdir(path);
+
     return git_remote_lookup(&REMOTE, REPOSITORY, "origin");
 }
 
@@ -213,7 +200,7 @@ int main(int args, char** argv) {
 
     struct stat repository_stat;
     stat(argv[1], &repository_stat);
-    if (!(repository_stat.st_mode & S_IFDIR)) {
+    if (!(repository_stat.st_mode & S_ISDIR(repository_stat.st_mode))) {
         printf("Path does not exist");
         exit(1);
     }
@@ -222,8 +209,10 @@ int main(int args, char** argv) {
     status = open_repository(argv[1]);
     setup_options();
     while (status == 0) {
-        printf("Watching..\n");
+        verbose_print("Watching..\n");
+        verbose_print("Fastforward..\n");
         status = do_fastforward();
+        verbose_print("Push..\n");
         status = do_push();
 
         git_repository_state_cleanup(REPOSITORY);
@@ -231,6 +220,6 @@ int main(int args, char** argv) {
     }
 
     const git_error *e = git_error_last();
-    printf("Error %d/%d: %s\n", status, e->klass, e->message);
+    fprintf(stderr, "Error %d/%d: %s\n", status, e->klass, e->message);
     return status;
 }
