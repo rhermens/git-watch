5994eefa575126cff55f88fdc55d99ae218d75ae
Roy Hermens <roy@debitroom.com>
2026-07-21 07:52:12 +0200

    simplify loop

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,6 @@ struct Args {
     interval: u64,
 }
 
-#[derive(Debug)]
-enum EventKind {
-    Tick(()),
-}
-
 fn path_expand(value: &str) -> Result<PathBuf, String> {
     Ok(PathBuf::from(
         shellexpand::full(value)
@@ -42,7 +37,6 @@ fn main() {
         .with_max_level(args.log_level)
         .init();
 
-    let (tx, rx) = mpsc::channel();
     let repo = Repository::open(&args.path).expect("Failed to open repository");
 
     let mut push_options = PushOptions::new();
@@ -55,31 +49,16 @@ fn main() {
     fetch_callbacks.credentials(credentials_callback);
     fetch_options.remote_callbacks(fetch_callbacks);
 
-    start_interval(tx.clone(), args.interval);
-
-    for e in rx {
-        tracing::trace!("Received event: {:?}", e);
-        match e {
-            EventKind::Tick(_) => {
-                tracing::trace!("Processing tick");
-                if let Err(e) = fast_forward(&repo, &mut fetch_options) {
-                    tracing::error!("error fast-forwarding: {}", e);
-                }
-
-                if let Err(e) = push_worktree(&repo, &mut push_options) {
-                    tracing::error!("error pushing: {}", e);
-                }
-            }
+    loop {
+        tracing::trace!("Processing tick");
+        if let Err(e) = fast_forward(&repo, &mut fetch_options) {
+            tracing::error!("error fast-forwarding: {}", e);
         }
+
+        if let Err(e) = push_worktree(&repo, &mut push_options) {
+            tracing::error!("error pushing: {}", e);
+        }
+
+        std::thread::sleep(Duration::from_secs(args.interval));
     }
-}
-
-fn start_interval(tx: mpsc::Sender<EventKind>, secs: u64) {
-    std::thread::spawn(move || {
-        loop {
-            std::thread::sleep(Duration::from_secs(secs));
-            tx.send(EventKind::Tick(()))
-                .expect("Failed to send interval tick");
-        }
-    });
 }
